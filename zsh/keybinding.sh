d1d23b33d63e9afb89e496bcdce6f8de66d97ff9
RamonYeung <capriceyhhu@gmail.com>
2019-04-10 17:47:41 +0800

    show the first indention line

--- zsh/keybinding.sh
+++ zsh/keybinding.sh
@@ -6,3 +6,14 @@ function up_widget() {
 zle -N up_widget         
 bindkey "^k" up_widget
 
+# git commit routinue
+function git_prepare() {
+    if [ -z "$BUFFER" ];
+	then
+	    BUFFER="git commit -v && git push"
+    fi
+    zle accept-line
+}
+zle -N git_prepare
+bindkey "^g" git_prepare
+
